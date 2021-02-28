9f02a1fc5c828d41b1bb8f2d6864bda12d078c04
KeithJoycemb <keithjoycemb@gmail.com>
2021-02-28 04:07:16 +0000

    Adding file reader and file writer
    Adding the bufferd reader and writer to the project for the course manager class

--- src/main/java/dkit/oop/CourseManager.java
+++ src/main/java/dkit/oop/CourseManager.java
@@ -1,6 +1,10 @@
 package dkit.oop;
 
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * CoursesManager
  * This software component Encapsulates the storage and management of
@@ -11,12 +15,55 @@ package dkit.oop;
  * course titles and institutions to the student.
  */
 
-public class CourseManager {
+public class CourseManager
+{
 
     // Store all the Course details.
     // Requires fast access given courseId.
+    private ArrayList<Course> CourseInfo;
 
-    public CourseManager() {
+    protected void loadCoursesFromFile()
+    {
+        try(Scanner coursesFile = new Scanner(new BufferedReader(new FileReader("courses.txt"))))
+        {
+            String input;
+            while(coursesFile.hasNextLine())
+            {
+                input = coursesFile.nextLine();
+                String[] data = input.split(",");
+                String courseId = data[0];
+                String level = data[1];
+                String title = data[2];
+                String institution = data[3];
+
+                Course readInCourse = new Course(courseId,level,title,institution);
+                this.CourseInfo.add(readInCourse);
+            }
+        }
+        catch(FileNotFoundException fne)
+        {
+            System.out.println(FontColours.RED + "System could not load in course information" + FontColours.RESET);
+        }
+    }
+
+    public void saveCoursesToFile()
+    {
+        try(BufferedWriter saveCoursesToFile = new BufferedWriter(new FileWriter("courses.txt")))
+        {
+            for(Course course : CourseInfo)
+            {
+                saveCoursesToFile.write(course.getCourseId() + "," + course.getLevel() + "," + course.getTitle() + "," + course.getInstitution());
+                saveCoursesToFile.write("\n");
+            }
+        }
+        catch(FileNotFoundException fne)
+        {
+            System.out.println(FontColours.RED + "System could not load in course information to file" + FontColours.RESET);
+        }
+    }
+
+    public CourseManager()
+    {
         // Hardcode some values to get started
         // load from text file "courses.dat" and populate coursesMap
     }
